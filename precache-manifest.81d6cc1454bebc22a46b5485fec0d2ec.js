bf65f576fc545f336a5ad35d13129c9a483f5e97
Elbert Ballo <eballofullstackdev@gmail.com>
2020-05-13 11:12:42 -0700

    Updates

---
+++ precache-manifest.81d6cc1454bebc22a46b5485fec0d2ec.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5565554a8cab45d6311c70c0424c2646",
+    "url": "/elberts-brokerage/index.html"
+  },
+  {
+    "revision": "cd7c7ef5b51d4479a612",
+    "url": "/elberts-brokerage/static/css/10.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "39027bdfc5ba6e2a3b1d",
+    "url": "/elberts-brokerage/static/css/11.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "6adfaab1d6d8e5d9a901",
+    "url": "/elberts-brokerage/static/css/12.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "be18e29d2c51bda0ef19",
+    "url": "/elberts-brokerage/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "0223d52a29d45ec4ad28",
+    "url": "/elberts-brokerage/static/css/6.5251fdc4.chunk.css"
+  },
+  {
+    "revision": "5004bb2649dbcec004e8",
+    "url": "/elberts-brokerage/static/css/7.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "1ae0032208ed711253a8",
+    "url": "/elberts-brokerage/static/css/8.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "d0623b40ba0195e50f5a",
+    "url": "/elberts-brokerage/static/css/9.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "7f937e84255ea2d12444",
+    "url": "/elberts-brokerage/static/css/main.8390ce8f.chunk.css"
+  },
+  {
+    "revision": "0894d23314f67968731a",
+    "url": "/elberts-brokerage/static/js/0.a5830166.chunk.js"
+  },
+  {
+    "revision": "5a3fea8fa1ce81a4b534",
+    "url": "/elberts-brokerage/static/js/1.f628bb20.chunk.js"
+  },
+  {
+    "revision": "cd7c7ef5b51d4479a612",
+    "url": "/elberts-brokerage/static/js/10.a42f93cf.chunk.js"
+  },
+  {
+    "revision": "39027bdfc5ba6e2a3b1d",
+    "url": "/elberts-brokerage/static/js/11.31371a85.chunk.js"
+  },
+  {
+    "revision": "6adfaab1d6d8e5d9a901",
+    "url": "/elberts-brokerage/static/js/12.390ccb29.chunk.js"
+  },
+  {
+    "revision": "e2af63578055497f97db",
+    "url": "/elberts-brokerage/static/js/13.cfee810c.chunk.js"
+  },
+  {
+    "revision": "ef5f32061ad3bcfc390e",
+    "url": "/elberts-brokerage/static/js/14.f0299f37.chunk.js"
+  },
+  {
+    "revision": "be18e29d2c51bda0ef19",
+    "url": "/elberts-brokerage/static/js/2.efae3a89.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/elberts-brokerage/static/js/2.efae3a89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e69aa05ddf7a4336f24",
+    "url": "/elberts-brokerage/static/js/3.a8d29266.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/elberts-brokerage/static/js/3.a8d29266.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0223d52a29d45ec4ad28",
+    "url": "/elberts-brokerage/static/js/6.41c11bfa.chunk.js"
+  },
+  {
+    "revision": "e89ad3dfb38ac973fcf2df05f27946de",
+    "url": "/elberts-brokerage/static/js/6.41c11bfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5004bb2649dbcec004e8",
+    "url": "/elberts-brokerage/static/js/7.e1a5bd60.chunk.js"
+  },
+  {
+    "revision": "1ae0032208ed711253a8",
+    "url": "/elberts-brokerage/static/js/8.c9946b95.chunk.js"
+  },
+  {
+    "revision": "d0623b40ba0195e50f5a",
+    "url": "/elberts-brokerage/static/js/9.83d85361.chunk.js"
+  },
+  {
+    "revision": "7f937e84255ea2d12444",
+    "url": "/elberts-brokerage/static/js/main.ad568546.chunk.js"
+  },
+  {
+    "revision": "1d0480f5d54c27a6543d",
+    "url": "/elberts-brokerage/static/js/runtime-main.09508805.js"
+  },
+  {
+    "revision": "8c20481d522a1bf8c654d2fcb5d769b2",
+    "url": "/elberts-brokerage/static/media/bribe.8c20481d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/elberts-brokerage/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/elberts-brokerage/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/elberts-brokerage/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/elberts-brokerage/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/elberts-brokerage/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
